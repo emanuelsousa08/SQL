951599c9274c63eec56daacf2782416e66bef21d
Emanuel Sousa <franciscoemanuel23@outlook.com>
2025-06-02 22:22:06 -0300

    Update SELECT from WORLD.sql

--- SELECT from WORLD.sql
+++ SELECT from WORLD.sql
@@ -23,3 +23,6 @@ WHERE area > 3000000 XOR population > 250000000
 
 SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000,2) FROM world
 WHERE continent = 'South America'
+
+SELECT name, ROUND(gdp / population, -3) FROM world  
+WHERE gdp >= 1000000000000;
